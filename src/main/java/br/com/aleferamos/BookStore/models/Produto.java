8804d2b281c1c0204fced2762c0f50088e9a9d5d
aleferamos <alefepdias@gmail.com>
2022-05-11 21:34:35 -0400

    Adicionando banco de dados

--- src/main/java/br/com/aleferamos/BookStore/models/Produto.java
+++ src/main/java/br/com/aleferamos/BookStore/models/Produto.java
@@ -1,4 +1,22 @@
 package br.com.aleferamos.BookStore.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "PRODUTO")
 public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "categoria")
+    private String categoria;
+
+    @Column(name = "codigo_barra")
+    private String codigoBarra;
+
+    @Column(name = "preco")
+    private double preco;
 }
